c5fb44285fbd5e1e9dfa93cc93c6e95808b2f30f
KrystianC-88 <krystiancierpial88@gmail.com>
2022-11-19 14:12:26 +0100

    canvas finally showing avatar

--- client/src/components/AvatarCanvas.tsx
+++ client/src/components/AvatarCanvas.tsx
@@ -1,19 +1,38 @@
 import React, {useEffect, useLayoutEffect, useRef} from 'react'
 import useQuery from 'react-query'
-import mergeImages from 'merge-images'
 import {ILayer} from '../interfaces/avatarInterface'
 
 function AvatarCanvas(layers:ILayer[]){
 
     console.warn(layers)
+    const avatarBase64 = [layers[0].parts[0], layers[1].parts[1], layers[2].parts[1]]
 
+    
+    
     //@ts-ignore
     const canvasRef = useRef<HTMLCanvasElement>();
     
-    useLayoutEffect(()=>{
-        //@ts-ignore
-        const context = canvasRef.current.getContext('2d');
-        
+    useEffect(()=>{
+        const canvasFunc = async () => {
+            // @ts-ignore
+            const context = canvasRef.current.getContext('2d');     
+            context?.clearRect(0, 0, context.canvas?.width, context.canvas?.height);
+
+            const AVATAR = avatarBase64.map(av => {
+                const img = new Image()
+                img.src = `data:image/png;base64,${av}`
+                return img
+            })
+            
+            AVATAR.forEach(part =>{
+                part.onload = () =>{
+                    context?.drawImage(part, 0, 0, context.canvas.width, context.canvas.height);
+                }
+            })
+            
+        }
+
+        canvasFunc()
         
     },[])
 
@@ -21,7 +40,7 @@ function AvatarCanvas(layers:ILayer[]){
     return(
         <>
             {/*//@ts-ignore*/}
-            <canvas ref={canvasRef}/>
+            <canvas ref={canvasRef} width="400" height="400"/>
             HELLO CANVAS
         </>
     )
